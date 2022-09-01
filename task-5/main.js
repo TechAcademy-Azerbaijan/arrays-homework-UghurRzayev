72d4df5919208bdda7b271b387f913aba0a0e949
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-01 20:24:23 +0400

    Task 5 done

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let numbers = result.input.split(" ")
+    let arr = []
+    for (let i = 0; i < numbers.length; i +=2) {
+        const element = parseInt(numbers[i]);
+        const next_element = parseInt(numbers[i+1])
+        if (next_element) {
+            arr.push(next_element)
+        } arr.push(element)
+
+    } console.log(arr.join(" "));
   
 });
