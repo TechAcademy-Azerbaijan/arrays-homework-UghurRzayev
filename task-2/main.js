b3e717165311ebccd2cc0a02628f6f7fc8db8708
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-01 19:35:01 +0400

    Task 2 done

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let numbers = result.input.split(" ")
+    let max = parseInt(numbers[0])
+    index = 0
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (element>=max) {
+            max=element
+            index=i+1
+        }
+    } console.log(`${max}\n${index}`);
   
 });
