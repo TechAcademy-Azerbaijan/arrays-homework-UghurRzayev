bef192e3eabfb7e00d18b64a77b956fae8dd5543
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-01 20:27:32 +0400

    Task 6 done

--- task-6/main.js
+++ task-6/main.js
@@ -4,6 +4,22 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let numbers = result.input.split(" ")
+    let max = parseInt(numbers[0])
+    let min = parseInt(numbers[0])
+    for (let i = 0; i< numbers.length; i++) {
+        const element = parseInt(numbers[i])   
+        if (element>max) {
+            max = element
+        } if (element<min) {
+            min = element
+        }
+    } let sum = 0
+    for (let i = 0; i< numbers.length; i++) {
+        const element = parseInt(numbers[i])   
+        if (element !== max &&element!==min) {
+            sum+=element
+        }
+    } console.log(sum);
   
 });
